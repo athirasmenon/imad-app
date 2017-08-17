a9aaa4fa1943eb9bb71671152c1d13ae2c1e39f3
athirasmenon <menonsathira@gmail.com>
2017-08-17 20:35:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('main_text'
 );
 element.innerHTML='new Value';
-var img=document.getElementById('madi'
-);
+var img=document.getElementById("madi");
 img.onclick=function(){
-  img.style. marginleft='100px';
+  img.style.marginleft='100px';
 };
